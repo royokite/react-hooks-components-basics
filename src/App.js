46822081cedb77f5354929eacde0dc1c41ee2659
RoyOkite <royokite@gmail.com>
2022-09-21 00:15:49 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Opinion from "./Opinion";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Opinion />
     </div>
   );
 }
---
+++ src/Opinion.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Opinion() {
+    return (
+        <React.Fragment>
+            <h3>This is what I honestly think:</h3>
+            <p>JSX is pretty neat!</p>
+        </React.Fragment>
+    )
+}
+
+export default Opinion
